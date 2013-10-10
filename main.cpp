0450bce3514d4e7a7e58563b00c403590b3ae024
hodgav <hodgavriel@gmail.com>
2013-10-10 22:57:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,14 +15,31 @@ int main()
   const char    *szTestString7 = NULL;
   const wchar_t *szTestString8 = NULL;
 
+  //Print the information which was asked in the exercise
   MyClass::printInformation(szTestString1);
   MyClass::printInformation(szTestString2);
+
+  //Begin printing my tests results
+  std::cout << std::endl;
+  std::cout << "Results of my custom tests:" << std::endl;
   MyClass::printInformation(szTestString3);
   MyClass::printInformation(szTestString4);
   MyClass::printInformation(szTestString5);
   MyClass::printInformation(szTestString6);
   MyClass::printInformation(szTestString7);
   MyClass::printInformation(szTestString8);
+
+  if(MyClass::getNiCount(szTestString7) == -1)
+	  std::cout << "szTestString7 is NULL, can not count number of Ni" << std::endl;
+
+  if(MyClass::replaceNiWithNI(szTestString7) == "")
+	  std::cout << "szTestString7 is empty or NULL, no need for replacements" << std::endl;
+
+  if(MyClass::getNiCount(szTestString8) == -1)
+	  std::cout << "szTestString8 is NULL, can not count number of Ni" << std::endl;
+
+  if(MyClass::replaceNiWithNI(szTestString8) == L"")
+	  std::cout << "szTestString8 is empty or NULL, no need for replacements" << std::endl;
 
   return 0;
 }
