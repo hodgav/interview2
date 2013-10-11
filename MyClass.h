29798af73c1e6d1d3c75a0875bdfbf10c8d0e0fe
hodgav <hodgavriel@gmail.com>
2013-10-11 16:43:13 +0200

    Update MyClass.h

--- MyClass.h
+++ MyClass.h
@@ -89,7 +89,7 @@ template <typename charType>
 static std::basic_string<charType> MyClass::replaceNiWithNI(const charType *src)
 {
 	//Throw an exception for a NULL pointer
-	if (src==NULL)
+	if (src == NULL)
 		throw std::invalid_argument("A null pointer was received (MyClass::replaceNiWithNI)");
 
     std::basic_string<charType> sourceString = src;
